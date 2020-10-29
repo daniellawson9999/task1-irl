fba1488573f1cab643e731ce7c8c77a2114cfaf7
Daniel Lawson <daniellawson9999@gmail.com>
2020-10-29 18:37:57 -0400

    updated files

--- MSC/generate_data_terran.py
+++ MSC/generate_data_terran.py
@@ -17,6 +17,7 @@ features interested in/indexes:
         Worker supply - 21
         Idea worker count - 22
 '''
+state_indexes = [16,17,18,19,20,21,22,23,24,25,26]
 feature_indexes = [20, 21, 22] # columns of each replay
 feature_signs = [1, 1, -1] # used map features -> rewards
 
@@ -43,11 +44,12 @@ for i in range(num_replays):
     index = str(i)
     data = np.asarray(sparse.load_npz(os.path.join(dir_path,paths[i])).todense())
     
-    # features -> states
-    features = data[:num_frames, feature_indexes]
-    states[index] = features
+
+    # store states
+    states[index] = data[:num_frames, state_indexes]
 
     # obtain rewards
+    features = data[:num_frames, feature_indexes]
     rewards[index] = features * feature_signs
 
     # append actions
@@ -58,9 +60,9 @@ save_directory = 'exported_replays'
 
 
 # make a descriptive file name
-states_file_name  = 'states_TerranVsTerran_{}_{}_{}'.format(num_replays,num_frames, feature_indexes)
+states_file_name  = 'states_TerranVsTerran_{}_{}_[{}:{}]'.format(num_replays,num_frames, state_indexes[0], state_indexes[-1])
 actions_file_name = 'actions_TerranVsTerran_{}_{}_{}'.format(num_replays,num_frames, terran_actions)
-rewards_file_name = 'rewards_TerranVsTerran_{}_{}_{}'.format(num_replays,num_frames, feature_signs)
+rewards_file_name = 'rewards_TerranVsTerran_{}_{}_{}'.format(num_replays,num_frames, np.array(feature_indexes) * feature_signs)
 
 states_file_path = os.path.join(save_directory, states_file_name)
 actions_file_path = os.path.join(save_directory, actions_file_name)
